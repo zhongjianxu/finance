83a57f9c4f8bee191ff82ec95adff6b7f4ef1885
zhongjianxu <16602053948@163.com>
2018-11-27 21:04:57 +0800

    增加依赖

--- src/main/java/com/zjx/finance/demo/controller/DemoController.java
+++ src/main/java/com/zjx/finance/demo/controller/DemoController.java
@@ -1,15 +1,15 @@
 package com.zjx.finance.demo.controller;
 
-
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 @Controller
 public class DemoController {
-
+	//private static Logger logger = Logger.getLogger(DemoController.class); 
 	@RequestMapping("/login")
 	public String hello() {
 		return "/login/login";
 	}
-	
 }
